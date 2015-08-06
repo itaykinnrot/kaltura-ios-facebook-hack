0203b7c19e3ecb13da32c6a6129bb71a0f0f5074
Clement Barry <clement.barry@oovoo.com>
2015-08-06 15:25:33 +0300

    Untested connectivity POC

--- WacthVideoPlayer WatchKit Extension/InterfaceController.m
+++ WacthVideoPlayer WatchKit Extension/InterfaceController.m
@@ -6,10 +6,11 @@
 //  Copyright © 2015 kaltura. All rights reserved.
 //
 
+@import WatchConnectivity;
 #import "InterfaceController.h"
 
 
-@interface InterfaceController()
+@interface InterfaceController() <WCSessionDelegate>
 @property (unsafe_unretained, nonatomic) IBOutlet WKInterfaceMovie *player;
 
 @end
@@ -17,14 +18,50 @@
 
 @implementation InterfaceController
 
+- (instancetype)init {
+    self = [super init];
+    
+    if (self) {
+        [WCSession defaultSession].delegate = self;
+        [[WCSession defaultSession] activateSession];
+    }
+    
+    return self;
+}
+
 - (void)awakeWithContext:(id)context {
     [super awakeWithContext:context];
-    NSURL *url = [[NSBundle mainBundle] URLForResource:@"mvcarchitecture" withExtension:@"mp4"];
-    [self.player setMovieURL:url];
-    [self presentMediaPlayerControllerWithURL:url options:nil completion:^(BOOL didPlayToEnd, NSTimeInterval endTime, NSError * _Nullable error) {
-        
-    }];
+//    [self.player setMovieURL:[self url]];
+//    [self presentMediaPlayerControllerWithURL:[self url] options:nil completion:^(BOOL didPlayToEnd, NSTimeInterval endTime, NSError * _Nullable error) {
+//        [self teleport:nil];
+//    }];
     // Configure interface objects here.
+}
+
+- (NSURL *)url
+{
+    return [[NSBundle mainBundle] URLForResource:@"mvcarchitecture" withExtension:@"mp4"];
+}
+
+- (IBAction)teleport:(id)sender
+{
+    WCSession *session = [WCSession defaultSession];
+    
+    if ([session isReachable])
+    {
+        NSDictionary *dictionary = @{ @"URL": [self url] };
+        [session sendMessage:dictionary
+                replyHandler:^(NSDictionary<NSString *, id> *replyMessage){
+                    NSLog(@"reply");
+                }
+                errorHandler:^(NSError *error){
+                    NSLog(@"error");
+                }];
+    }
+    else
+    {
+        NSLog(@"session not reachable");
+    }
 }
 
 - (void)willActivate {
